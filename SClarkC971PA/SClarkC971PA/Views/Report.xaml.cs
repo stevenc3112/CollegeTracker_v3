67ac3ebf1fa3a2913b1d4103df797800655ee16d
stkeven <steventtclark@gmail.com>
2025-10-20 20:05:26 -0600

    Added search bar and some conditional statements

--- SClarkC971PA/SClarkC971PA/Views/Report.xaml.cs
+++ SClarkC971PA/SClarkC971PA/Views/Report.xaml.cs
@@ -8,6 +8,8 @@ public partial class Report : ContentPage
 	private int _currentUserId = 0;
 	private string _assessmentType = "";
 	private string _assessmentStatus = "";
+	private IEnumerable<Assessment> _originalReport;
+	private IEnumerable<Assessment> _reportSearchResult;
 	public Report(int currentUserId)
 	{
 		InitializeComponent();
@@ -26,13 +28,17 @@ public partial class Report : ContentPage
 			_assessmentStatus = AssessmentStatusPkr.SelectedItem.ToString();
             if (_assessmentType == "Performance")
             {
-				AssessmentCollectionView.ItemsSource = await DatabaseService.GetPerformanceAssessmentReport(_currentUserId, _assessmentStatus);
+                _originalReport = await DatabaseService.GetPerformanceAssessmentReport(_currentUserId, _assessmentStatus);
+				_reportSearchResult = Enumerable.Empty<Assessment>();
+                AssessmentCollectionView.ItemsSource = _originalReport;
 				ReportErrorLbl.Text = "Report run " + DateTime.Now.ToString("MM/dd/yyyy 'at' hh:mm tt");
 				ReportErrorLbl.IsVisible = true;
             }
 			else if (_assessmentType == "Objective")
 			{
-                AssessmentCollectionView.ItemsSource = await DatabaseService.GetObjectiveAssessmentReport(_currentUserId, _assessmentStatus);
+                _originalReport = await DatabaseService.GetObjectiveAssessmentReport(_currentUserId, _assessmentStatus);
+                _reportSearchResult = Enumerable.Empty<Assessment>();
+                AssessmentCollectionView.ItemsSource = _originalReport;
                 ReportErrorLbl.Text = "Report run " + DateTime.Now.ToString("MM/dd/yyyy 'at' hh:mm tt");
                 ReportErrorLbl.IsVisible = true;
             }
@@ -43,5 +49,81 @@ public partial class Report : ContentPage
 			ReportErrorLbl.Text = "Assessment Status and Type cannot be empty fields.";
 			ReportErrorLbl.IsVisible = true;
 		}
+    }
+
+  //  private void ReportSearchBar_SearchButtonPressed(object sender, EventArgs e)
+  //  {
+		//if (string.IsNullOrEmpty(ReportSearchBar.Text))
+		//{
+		//	AssessmentCollectionView.ItemsSource = _originalReport;
+		//}
+		//else
+		//{
+		//	string searchKeyword = ReportSearchBar.Text.ToString();
+		//	foreach (Assessment assessment in _originalReport)
+		//	{
+		//		if (Int32.TryParse(searchKeyword, out int assessmentId))
+		//		{
+		//			if (assessment.AssessmentId == assessmentId && !_reportSearchResult.Contains(assessment))
+		//			{
+		//				_reportSearchResult.Append(assessment);
+		//			}
+		//		}
+		//		if (DateTime.TryParse(searchKeyword, out DateTime searchDate))
+		//		{
+		//			if (!_reportSearchResult.Contains(assessment) && assessment.AssessmentStartDate == searchDate || assessment.AssessmentEndDate == searchDate)
+		//			{
+		//				_reportSearchResult.Append(assessment);
+		//			}
+		//		}
+		//		if (!_reportSearchResult.Contains(assessment) && assessment.AssessmentStatus.ToString().ToLower().Contains(searchKeyword.ToLower()))
+		//		{
+		//			_reportSearchResult.Append(assessment);
+		//		}
+		//		if (!_reportSearchResult.Contains(assessment) && assessment.AssessmentName.ToString().ToLower().Contains(searchKeyword.ToLower()))
+		//		{
+		//			_reportSearchResult.Append(assessment);
+		//		}
+		//		AssessmentCollectionView.ItemsSource = _reportSearchResult;
+  //          }
+		//}
+  //  }
+
+    private void ReportSearchBar_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(ReportSearchBar.Text))
+        {
+            AssessmentCollectionView.ItemsSource = _originalReport;
+        }
+        else
+        {
+            string searchKeyword = ReportSearchBar.Text.ToString();
+            foreach (Assessment assessment in _originalReport)
+            {
+                if (Int32.TryParse(searchKeyword, out int assessmentId))
+                {
+                    if (assessment.AssessmentId == assessmentId && !_reportSearchResult.Contains(assessment))
+                    {
+                        _reportSearchResult.Append(assessment);
+                    }
+                }
+                if (DateTime.TryParse(searchKeyword, out DateTime searchDate))
+                {
+                    if (!_reportSearchResult.Contains(assessment) && assessment.AssessmentStartDate == searchDate || assessment.AssessmentEndDate == searchDate)
+                    {
+                        _reportSearchResult.Append(assessment);
+                    }
+                }
+                if (!_reportSearchResult.Contains(assessment) && assessment.AssessmentStatus.ToString().ToLower().Contains(searchKeyword.ToLower()))
+                {
+                    _reportSearchResult.Append(assessment);
+                }
+                if (!_reportSearchResult.Contains(assessment) && assessment.AssessmentName.ToString().ToLower().Contains(searchKeyword.ToLower()))
+                {
+                    _reportSearchResult.Append(assessment);
+                }
+                AssessmentCollectionView.ItemsSource = _reportSearchResult;
+            }
+        }
     }
 }
