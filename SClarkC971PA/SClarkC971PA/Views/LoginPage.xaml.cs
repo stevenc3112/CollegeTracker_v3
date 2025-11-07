7da7744d4a0e2bedd1cf27e09d2b522732e200c0
stevenc3112 <steven.clark@power-innovations.com>
2025-11-07 14:27:10 -0800

    Update LoginPage.xaml.cs

--- SClarkC971PA/SClarkC971PA/Views/LoginPage.xaml.cs
+++ SClarkC971PA/SClarkC971PA/Views/LoginPage.xaml.cs
@@ -1,7 +1,7 @@
 using SClarkC971PA.Services;
 using SClarkC971PA.Models;
 namespace SClarkC971PA.Views;
-
+//Test that maintenance instructions work
 public partial class LoginPage : ContentPage
 {
     private User _user = new User();
